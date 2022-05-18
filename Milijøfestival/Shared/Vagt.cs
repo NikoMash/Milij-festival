6451dab328d51961320e6d4d5cb18f46670595d3
frederiklindhard <frederik0203@gmail.com>
2022-05-18 14:46:36 +0200

    Oprettelse af vagtklasse

--- Milijøfestival/Shared/Vagt.cs
+++ Milijøfestival/Shared/Vagt.cs
@@ -3,8 +3,26 @@ namespace Milijøfestival.Shared
 {
     public class Vagt
     {
-        public Vagt()
+        //Properties
+
+        public int VagtId { get; set; }
+
+        public DateTime Tid { get; set; }
+
+        public string Sted { get; set; }
+
+        public string Afdeling { get; set; }
+
+
+        //Constructer
+        public Vagt(int vagtid, DateTime tid, string sted, string afdeling)
         {
+            VagtId = vagtid;
+            Tid = tid;
+            Sted = sted;
+            Afdeling = afdeling;
         }
+
     }
+
 }

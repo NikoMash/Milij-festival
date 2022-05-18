8819adc70a21094da743cddbee1513d505c706c5
Nikolaj Sievertsen Nøring <nikosn@hotmail.dk>
2022-05-18 14:46:43 +0200

    f

--- Milijøfestival/Shared/Bruger.cs
+++ Milijøfestival/Shared/Bruger.cs
@@ -11,7 +11,7 @@ namespace Milijøfestival.Shared
         public int Alder { get; set; }
 
         //Constructor
-        public Bruger(int brugerid, string navn, int telefonnr, string email, int alder) : base (brugerid, navn, ...)
+        public Bruger(int brugerid, string navn, int telefonnr, string email, int alder)
         {
             BrugerId = brugerid;    
             Navn = navn;    

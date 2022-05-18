628154456eec0c66de1b0b2de3de2bf967e8247b
jacobriisgaard <eaajrii@students.eaaa.dk>
2022-05-18 14:46:26 +0200

    oprettelse af frivllig klasse

--- Milijøfestival/Shared/Frivillig.cs
+++ Milijøfestival/Shared/Frivillig.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 namespace Milijøfestival.Shared
 {
-    public class Frivillig
+    public class Frivillig : Bruger
     {
-        public Frivillig()
+        //properties
+        public int FrivilligId { get; set; }
+
+        public string Kompetencer { get; set; }
+
+
+        //constructer
+        public Frivillig(int frivilligid, string kompetencer, int brugerid, string telefonnr, string email, int alder) : base (brugerid, navn, telefonnr, email, alder)
         {
+            FrivilligId = frivilligid;
+            Kompetencer = kompetencer;
+            
         }
     }
 }

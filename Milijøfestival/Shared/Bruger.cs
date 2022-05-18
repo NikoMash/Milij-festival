533a06317021e0c017341febd767513686f37c7f
Nikolaj Sievertsen Nøring <nikosn@hotmail.dk>
2022-05-18 14:44:05 +0200

    Bruger klasse

--- Milijøfestival/Shared/Bruger.cs
+++ Milijøfestival/Shared/Bruger.cs
@@ -3,8 +3,27 @@ namespace Milijøfestival.Shared
 {
     public class Bruger
     {
-        public Bruger()
+        //properties
+        public int BrugerId { get; set; }
+        public string Navn { get; set; }
+        public int TelefonNr { get; set; }
+        public string Email { get; set; }
+        public int Alder { get; set; }
+
+        //Constructor
+        public Bruger(int brugerid, string navn, int telefonnr, string email, int alder) : base (brugerid, navn, ...)
         {
+            BrugerId = brugerid;    
+            Navn = navn;    
+            TelefonNr = telefonnr;
+            Email = email;
+            Alder = alder;
         }
+
+
+
+
+
+
     }
 }

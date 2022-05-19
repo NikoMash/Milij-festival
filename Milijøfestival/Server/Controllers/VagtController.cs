fc460976bb39ec0caf1e31917ba65900d7028f12
Nikolaj Sievertsen Nøring <nikosn@hotmail.dk>
2022-05-19 12:05:48 +0200

    VagtController

---
+++ Milijøfestival/Server/Controllers/VagtController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Milijøfestival.Server.Controllers
+{
+    public class VagtController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

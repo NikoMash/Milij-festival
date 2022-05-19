9bbc9aae2ecf7a32d59e25c92a6930015f67ec56
Nikolaj Sievertsen Nørring <eaansno@students.eaaa.dk>
2022-05-19 21:10:22 +0200

    Mulig data

---
+++ Milijøfestival/Shared/HentVagtData.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Npgsql;
+
+namespace Milijøfestival.Shared
+{
+
+    public class HentVagtData : IHentVagtData
+    {
+
+
+        private readonly SqlConnectionConfiguration _configuration;
+        public HentVagtData(SqlConnectionConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        //Henter en liste af vagter (Select / Read)
+        public async Task<IEnumerable<Vagt>> VagtList()
+        {
+            var selectstedvagter = "Select sted FROM vagt";
+            IEnumerable<Vagt> vagter;
+            using (var connection = new NpgsqlConnection("SuperUser"))
+            {
+                vagter = await connection.QueryAsync<Vagt>(selectstedvagter);
+                /*
+            ("spVagt_GetAll", commandType: CommandType.StoredProcedure);
+                */
+            }
+            return vagter.ToList();
+        }
+
+    }
+}
---
+++ Milijøfestival/Shared/IHentVagtData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Milijøfestival.Shared
+{
+    public interface IHentVagtData
+    {
+        Task<IEnumerable<Vagt>> VagtList();
+    }
+}

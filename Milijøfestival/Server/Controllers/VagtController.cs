82cb2fd0d66a0f4584650259f9e01d7c4d952d3e
Nikolaj Sievertsen Nøring <nikosn@hotmail.dk>
2022-05-23 12:48:58 +0200

    Oprette vagter

--- Milijøfestival/Server/Controllers/VagtController.cs
+++ Milijøfestival/Server/Controllers/VagtController.cs
@@ -1,29 +1,48 @@
 ﻿using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Milijøfestival.Shared;
 using Npgsql;
+using System;
 using System.Threading.Tasks;
 
 namespace Milijøfestival.Server.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class VagtController : Controller
     {
+        private readonly ILogger<VagtController> _logger;
+
+        public VagtController(ILogger<VagtController> logger)
+        {
+            _logger = logger;
+        }
+
         NpgsqlConnection connection = new NpgsqlConnection("UserID=postgres; Password = Kulturkongerne2022; Host = milijofestival.postgres.database.azure.com; Port = 5432; Database = milijofestival; ");
 
-        public async Task Add(Vagt nyvagt)
+        public async Task<ActionResult<Vagt>> PutTask(Vagt nyvagt)
         {
 
             connection.Open();
-            string opretvagt = "INSERT INTO vagt (starttid, sluttid, afdeling, sted, opgid) VALUES (@starttid, @sluttid, @afdeling, @sted, @opgid)";
-            var vagtArgumenter = new
+            try
             {
-                starttid = nyvagt.StartTid,
-                sluttid = nyvagt.SlutTid,
-                afdeling = nyvagt.Afdeling,
-                sted = nyvagt.Sted,
-                opgid = nyvagt.OpgId
-            };
-            await connection.ExecuteAsync(opretvagt, vagtArgumenter);
+                string opretvagt = "INSERT INTO vagt (starttid, sluttid, afdeling, sted, opgid) VALUES (@starttid, @sluttid, @afdeling, @sted, @opgid)";
+                var vagtArgumenter = new
+                {
+                    starttid = nyvagt.StartTid,
+                    sluttid = nyvagt.SlutTid,
+                    afdeling = nyvagt.Afdeling,
+                    sted = nyvagt.Sted,
+                    opgid = nyvagt.OpgId
+                };
+                await connection.ExecuteAsync(opretvagt, vagtArgumenter);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+            return Ok(nyvagt);  
         }
     }
 }

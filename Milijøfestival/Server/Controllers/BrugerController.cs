23b05bc55b8ff9e4f8dd2b948a47d14ca1fcc181
frederiklindhard <frederik0203@gmail.com>
2022-05-27 12:47:33 +0200

    Knap med Opret bruger

--- Milijøfestival/Server/Controllers/BrugerController.cs
+++ Milijøfestival/Server/Controllers/BrugerController.cs
@@ -18,15 +18,17 @@ namespace Milijøfestival.Server.Controllers
         {
             _logger = logger;
         }
-
+        // Forbinder til databasen
         NpgsqlConnection connection = new NpgsqlConnection("UserID=postgres; Password = Kulturkongerne2022; Host = milijofestival.postgres.database.azure.com; Port = 5432; Database = milijofestival; ");
 
+        // Forbinder med Razorpage og henter intastet data
         public async Task<ActionResult<Bruger>> PutTask(Bruger nybruger)
         {
 
             connection.Open();
             try
             {
+                // laver quiry for at indsætte intastet data i databasen
                 string opretbruger = "INSERT INTO bruger (navn, telefonnr, email, fødselsdato, rolleid) VALUES (@navn, @telefonnr, @email, @fødselsdato, @rolleid)";
                 var brugerArgumenter = new
                 {

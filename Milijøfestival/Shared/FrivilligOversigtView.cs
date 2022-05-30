bf40de76cf869f27bd8b3a7b7ff9456de05421e9
Nikolaj Sievertsen Nøring <nikosn@hotmail.dk>
2022-05-30 15:22:57 +0200

    frivillig

--- Milijøfestival/Shared/FrivilligOversigtView.cs
+++ Milijøfestival/Shared/FrivilligOversigtView.cs
@@ -14,20 +14,18 @@ namespace Milijøfestival.Shared
 
         public string RolleNavn { get; set; }
 
-        public string Kompetence { get; set; }
 
 
 
 
         //constructor
-        public FrivilligOversigtView(string navn, string telefonnr, string email, DateTime fødselsdato, string rollenavn, string kompetence)
+        public FrivilligOversigtView(string navn, string telefonnr, string email, DateTime fødselsdato, string rollenavn)
         {
             Navn = navn;
             TelefonNr = telefonnr;
             Email = email;
             Fødselsdato = fødselsdato;
             RolleNavn = rollenavn;
-            Kompetence = kompetence;
 
         }
     }
